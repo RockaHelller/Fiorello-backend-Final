14e94e6995acd5b7236f1f1c4d62ddc9f1ef7481
Murad Jafarov <muradkk@code.edu.az>
2023-06-23 00:23:54 +0400

    Add project files.

---
+++ Fiorello-backend/Migrations/20230622185757_Created.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fiorello_backend.Migrations
+{
+    public partial class Created : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 22, 22, 57, 57, 457, DateTimeKind.Local).AddTicks(1368));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 22, 22, 57, 57, 457, DateTimeKind.Local).AddTicks(1376));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 22, 22, 57, 57, 457, DateTimeKind.Local).AddTicks(1377));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 11, 13, 57, 8, 715, DateTimeKind.Local).AddTicks(932));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 11, 13, 57, 8, 715, DateTimeKind.Local).AddTicks(1002));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 11, 13, 57, 8, 715, DateTimeKind.Local).AddTicks(1004));
+        }
+    }
+}
